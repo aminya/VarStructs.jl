859d8d35e9b963e3c2803b259acb4631d11d9e2c
aminya <aminyahyaabadi74@gmail.com>
2020-04-29 02:46:52 -0500

    varTdefVal parse

--- src/parse.jl
+++ src/parse.jl
@@ -34,3 +34,17 @@ function varT(input)
     return args_type, args_field, args_param, exprargsI
 end
 
+"""
+Parse `var::T = defVal` or `var = defVal`
+"""
+function varTdefVal(input)
+    input.head == :(kw) || input.head == :(=) || throw(ArgumentError("Input should be in form of `var/var::T = defVal`. You passed $input"))
+
+    lhs = input.args[1] # var or var::T
+    args_type, args_field, args_param, exprargsI = varT(lhs)
+
+    args_defaultvalue = input.args[2] # defVal
+    args_param = Expr(:kw, args_field, args_defaultvalue)
+
+    return args_defaultvalue, args_type, args_param, args_field, exprargsI
+end

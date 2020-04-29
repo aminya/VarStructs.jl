f764f67580a7708ad3d475ead96c468a93134bcd
aminya <aminyahyaabadi74@gmail.com>
2020-04-29 02:45:53 -0500

    Props

--- src/VarStructs.jl
+++ src/VarStructs.jl
@@ -11,6 +11,24 @@ typeof(f) <: VarStructs.Name # true
 """
 Name = Union{Symbol, String}
 
+"""
+A struct to hold the information about a VarStruct field
+
+# Example
+```julia
+fp = Props(20.0, Float64)
+```
+"""
+mutable struct Props{T}
+    value::Union{Missing, T} # missing when the value is not provided yet, but the type is known
+    type::Type{T}
+    # check::Union{Function, Nothing}
+end
+
+function Props(value::Union{Missing, T}) where T
+    return Props{T}(value, T)
+end
+
 
 
 
